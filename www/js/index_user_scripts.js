3bc4c755407bf031c091742dc58d2dd69710cf61
theauda <brionmathieu@gmail.com>
2016-05-21 15:12:54 +0200

    commit prefinal mathieu

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -63,7 +63,12 @@
 /* button  Enigme 2.2 */    $(document).on("click", ".uib_w_33", function(evt)     {chargerEnigme(12); });
 /* button  Enigme 2.3 */    $(document).on("click", ".uib_w_34", function(evt)     {chargerEnigme(13); });
 /* button  Enigme 2.4 */    $(document).on("click", ".uib_w_35", function(evt)     {chargerEnigme(14); });
-    
+ 
+
+/* button  Sortir */    $(document).on("click", ".uib_w_86", function(evt)    {             return false;    });  
+ 
+ 
+ 
 /* button  #Validation1 */
     $(document).on("click", "#Validation1", function(evt)
     { var texte=document.getElementById("Saisie_Texte_Texte").value ;
@@ -109,39 +114,100 @@ function suivant()  {addCurrent();addDebloquer();chargerEnigme(getCurrent());   
     
 function getEgnime(Id) {
     var texte="Enigme "+Id+" :<br>";
-    texte+=" Reponse = "+getReponse(Id)+" <br> ";           
-        
-        switch(Id)
-            {case 0:return texte+"b8 88 ? 98";
-                    
-            case 1:return "images/Arrow.png";
+    
+    if(Id<=10){//---------------------- Chapitre 1---------------------------------------------------------------------------------------------------  
+            switch(Id)
+            {case 0:return texte+"b8 88 ? 98";               
+            case 1:return "images/Enigme_1_1.png";
                     
             case 2:return texte+"64 chiens sont inscrits pour un concours de beauté canin réservé aux chiens de races, parmi ces 64 chiens de races, 50 % arrivent en huitième de finale. Lors des de cette épreuve, 50% sont éliminés, arrivent les quarts où,face à un jury énervé par le temps pluvieux, encore 50% sont éliminés. Finalement, après des heures de délibérations,le jury décide enfin qui sera envoyé en finale. Parmi les gagnants, quel pourcentage à gagné ?";
                     
-            case 3:return  texte+"Ayant réglée mon réveil analogique pour 10 heures du matin, et comptant dormir vers disons 8h du soir, dans combien d'heure sortirais-je de veille ?";  
-                    
-            case 4:return texte+"Combien de gouttes d'eau peut-on mettre dans un verre vide ?";  
-                    
-            case 5:return texte+"Qu'obtient-on si l'on mélange du Calcium, du Nickel, du Carbone et de l'Hélium ?";
-                    
-            case 6:return texte+"Trouvez l'errreur : 9 7 5 6 2 3 1 8 4";
-                    
-            case 7:return texte+"Quel nombre divisé par lui-même donne son double ?"; 
-                    
-            case 8:return texte+"Si développer une I.A coutait 2000 €,Le support électronique coutant 1990 € de plus que le programme,, combien coute le programme ?";
-                    
+            case 3:return  texte+"Ayant réglée mon réveil analogique pour 10 heures du matin, et comptant dormir vers disons 8h du soir, dans combien d'heure sortirais-je de veille ?";           
+            case 4:return texte+"Combien de gouttes d'eau peut-on mettre dans un verre vide ?";                      
+            case 5:return texte+"Qu'obtient-on si l'on mélange du Calcium, du Nickel, du Carbone et de l'Hélium ?";                  
+            case 6:return texte+"Trouvez l'errreur : 9 7 5 6 2 3 1 8 4";                    
+            case 7:return texte+"Quel nombre divisé par lui-même donne son double ?";                     
+            case 8:return texte+"Si développer une I.A coutait 2000 €,Le support électronique coutant 1990 € de plus que le programme,, combien coute le programme ?";                   
             case 9:return texte+"MEDENTREN";
                     
             case 10:return texte+"Un train A part de Paris vers Bordeaux à 7h00 du matin et roule à 90 km/h. Le même jour à 8h00, un autre train B part de Bordeaux vers Paris et roule à 110 km/h. Lorsqu'ils se croiseront, lequel de ces trains sera le plus proche de Paris ?";
-                    
-                    
-            default:return texte+"limite depasser";     
-            }       
-    
         
+            default:return texte+"limite depasser";  }}
+    else if (Id<=20)
+        {switch((Id-10))
+            {  //---------------------- Chapitre 2---------------------------------------------------------------------------------------------------  
+            case 1:return texte+"Lyha pèse 2,5 Ko et la moitié de son poid. Combien pèse-telle de bits?" ;
+            case 2:return "images/Arrow.png";
+            case 3:return "images/Arrow.png";
+            case 4:return texte+"limite depasser" ;
+            case 5:return texte+"limite depasser" ;
+            case 6:return texte+"limite depasser" ;
+            case 7:return texte+"limite depasser" ;
+            case 8:return texte+"limite depasser" ;
+            case 9:return texte+"limite depasser" ;
+            case 10:return texte+"3011<br/>3112 <br/> 3101<br/> 2802<br/> 3103<br/> 3004<br/> ????" ;
+   
+            default:return texte+"limite depasser";     
+            }
+        }
+       else if(Id<=20)
+        {switch((Id-10))
+            {  //---------------------- Chapitre 3---------------------------------------------------------------------------------------------------  
+            case 1:return texte+"Lyha pèse 2,5 Ko et la moitié de son poid. Combien pèse-telle de bits?" ;
+            case 2:return "images/Arrow.png";
+            case 3:return "images/Arrow.png";
+            case 4:return texte+"limite depasser" ;
+            case 5:return texte+"limite depasser" ;
+            case 6:return texte+"limite depasser" ;
+            case 7:return texte+"limite depasser" ;
+            case 8:return texte+"limite depasser" ;
+            case 9:return texte+"limite depasser" ;
+            case 10:return texte+"limite depasser" ;
+   
+            default:return texte+"limite depasser";     
+            }
+        }
+        else if(Id<=30)
+        {switch((Id-20))
+            {  //---------------------- Chapitre 4---------------------------------------------------------------------------------------------------  
+            case 1:return texte+"limite depasser" ;
+            case 2:return texte+"limite depasser" ;
+            case 3:return texte+"limite depasser" ;
+            case 4:return texte+"limite depasser" ;
+            case 5:return texte+"limite depasser" ;
+            case 6:return texte+"limite depasser" ;
+            case 7:return texte+"limite depasser" ;
+            case 8:return texte+"limite depasser" ;
+            case 9:return texte+"limite depasser" ;
+            case 10:return texte+"limite depasser" ;
+   
+            default:return texte+"limite depasser";     
+            }
+        }
+        else if(Id<=40)
+        {switch((Id-30))
+            {  //---------------------- Chapitre 5---------------------------------------------------------------------------------------------------  
+            case 1:return texte+"limite depasser" ;
+            case 2:return texte+"limite depasser" ;
+            case 3:return texte+"limite depasser" ;
+            case 4:return texte+"limite depasser" ;
+            case 5:return texte+"limite depasser" ;
+            case 6:return texte+"limite depasser" ;
+            case 7:return texte+"limite depasser" ;
+            case 8:return texte+"limite depasser" ;
+            case 9:return texte+"limite depasser" ;
+            case 10:return texte+"limite depasser" ;
+   
+            default:return texte+"limite depasser";     
+            }
+         return "limite depasser" ;
+        }    
     }
+  
      function getReponse(Id) {
-        switch(Id)
+         
+         if(Id<=10){//---------------------- Chapitre 1---------------------------------------------------------------------------------------------------
+                 switch(Id)
             {case 0:return "87";
             case 1:return "16";
             case 2:return "100";
@@ -153,26 +219,43 @@ function getEgnime(Id) {
             case 8:return "5";
             case 9:return "rendement";
             case 10:return "aucun";
+          
              default:return "limite depasser";     
             }       
-     }   
+     }  
+         
+         else if(Id<=20){//---------------------- Chapitre 2---------------------------------------------------------------------------------------------------
+                   switch((Id-10))
+            {
+            case 1:return "40960" ;
+            case 2:return "3113" ;
+            case 3:return "2561" ;
+            case 4:return "limite depasser" ;
+            case 10:return "limite depasser" ;
+             default:return "limite depasser";     
+            } 
+         }
+         else if(Id<=30){//---------------------- Chapitre 3---------------------------------------------------------------------------------------------------
+         }
+         else if(Id<=40){//---------------------- Chapitre 4---------------------------------------------------------------------------------------------------
+         }
+    return "limite depasser" ;     
+}
+     
     
-    function charger_Texte_Texte(ID)
-    {document.getElementById("Enoncer_Texte_Texte").innerHTML=getEgnime(ID) ;}
-    
-     function charger_Image_Texte(ID)
-    {document.getElementById("Image_Texte").setAttribute("src", getEgnime(ID));;
-  
-       
-    }
+    function charger_Texte_Texte(ID)    {document.getElementById("Enoncer_Texte_Texte").innerHTML=getEgnime(ID) ;}
+    function charger_Image_Texte(ID)    {document.getElementById("afficher_Image_Texte").setAttribute("src", getEgnime(ID));}
     
     
     function getType(Id)
     { switch(Id)        /*type:1 => texte texte               2 => image texte        */
-            {case 0:return 1;           case 1:return 2;            case 2:return 1;            case 3:return 1;            case 4:return 1;
-            case 5: return 1;            case 6:return 1;            case 7:return 1;            case 8:return 1;            case 9:return 1;
-            case 10:return 1;             
-                
+            {case 0:return 1;   case 1:return 2;    case 2:return 1;
+             case 3:return 1;   case 4:return 1;    case 5:return 1;
+             case 6:return 1;   case 7:return 1;    case 8:return 1;
+             case 9:return 1;   case 10:return 1; 
+                    
+             case 11:return 1;  case 12:return 2;  case 13:return 3;
+             case 14:return 1;  case 15:return 1;  case 16:return 1;
             default:return 1;     
             } 
     }
@@ -180,22 +263,17 @@ function getEgnime(Id) {
     function chargerPaterne(Id)
     {switch(getType(Id))
             {   case 1:charger_Texte_Texte(Id);         activate_subpage("#Texte_Texte"); break;     
-                case 2:charger_Texte_Texte(Id);         activate_subpage("#Image_Texte"); break;      
-                case 3: break;    
-                case 4: break;  
+                case 2:charger_Image_Texte(Id);         activate_subpage("#Image_Texte"); break;      
+                
                 default:break;
             }     
     }
-      /* button  Sortir */
-    $(document).on("click", ".uib_w_86", function(evt)
-    {
-       myApp.exitApp();
-         return false;
-    });
-    
-    }
-    
-    
+  
+  
+
+ 
+ }
+ 
     
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
